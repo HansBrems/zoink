be64806ddc8251ac04fac9ef567c3dad5c99788c
Hans Brems <hbrems@Darkon.local>
2023-12-12 22:59:14 +0100

    Refactoring

--- src/components/player.ts
+++ src/components/player.ts
@@ -4,6 +4,8 @@ import { PlayerKeys } from '~/models/playerKeys';
 const PLAYER_SPEED = 80;
 
 export default class Player extends Phaser.Physics.Arcade.Sprite {
+  playerKeys: PlayerKeys;
+
   constructor(
     scene: Phaser.Scene,
     x: number,
@@ -15,30 +17,43 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
     scene.add.existing(this);
     scene.physics.add.existing(this);
-    this.body.setSize(this.width * 0.3, this.height * 0.5);
-
+    this.body!.setSize(this.width * 0.3, this.height * 0.5);
+    this.playerKeys = this.createPlayerKeys(scene.input.keyboard!);
     this.playIdleAnim();
+    scene.cameras.main.startFollow(this, true);
   }
 
-  public updatePosition(playerKeys: PlayerKeys) {
-    if (playerKeys.a.isDown) {
+  public updatePosition() {
+    if (this.playerKeys.a.isDown) {
       this.setVelocity(-PLAYER_SPEED, 0);
       this.anims.play('player-walk-right', true);
       this.flipX = true;
-    } else if (playerKeys.d.isDown) {
+    } else if (this.playerKeys.d.isDown) {
       this.setVelocity(PLAYER_SPEED, 0);
       this.anims.play('player-walk-right', true);
       this.flipX = false;
-    } else if (playerKeys.w.isDown) {
+    } else if (this.playerKeys.w.isDown) {
       this.setVelocity(0, -PLAYER_SPEED);
       this.anims.play('player-walk-up', true);
-    } else if (playerKeys.s.isDown) {
+    } else if (this.playerKeys.s.isDown) {
       this.setVelocity(0, PLAYER_SPEED);
       this.anims.play('player-walk-down', true);
     } else {
       this.playIdleAnim();
       this.setVelocity(0, 0);
     }
+  }
+
+  private createPlayerKeys(
+    keyboard: Phaser.Input.Keyboard.KeyboardPlugin,
+  ): PlayerKeys {
+    return {
+      cursorKeys: keyboard!.createCursorKeys(),
+      a: keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+      s: keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+      d: keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+      w: keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+    };
   }
 
   private playIdleAnim() {
